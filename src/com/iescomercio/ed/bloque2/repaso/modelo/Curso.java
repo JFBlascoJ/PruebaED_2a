4d72e6f128ba827fc8a4b55f36ba5b2c16ac10d8
jfblascoj <jfblascoj@gmail.com>
2022-02-13 16:51:32 +0100

    Documentada clase Curso

--- src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
@@ -7,9 +7,11 @@ public class Curso {
 	
 	private List<Persona> listaAlumnos;
 
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Elimina del curso a la persona con el DNI indicado por usuario
+	 * @param dni DNI de la persona a borrar
+	 * @throws Exception cuando el DNI no tiene la longitud adecuada
+	 */
 	public void eliminarAlumno(String dni) throws Exception {
 		if(dni.length()==9) {//comprobar la longitud del dni
 			listaAlumnos.remove(new Persona(dni, "", "")); //solo hace falta el dni
@@ -18,16 +20,19 @@ public class Curso {
 		}
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Añade una nueva persona al curso
+	 * @param p Persona a añadir
+	 */
 	public void aniadirAlumno(Persona p) {
 		listaAlumnos.add(p);
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Comprueba si el DNI pasado por parámetro existe dentro del curso en cuestión
+	 * @param dni DNI a comprobar
+	 * @return True si existe. En caso contratio, retornará false
+	 */
 	public Boolean estaRegistrado(String dni) {
 		int i =0;
 		Boolean encontrado=false;
@@ -40,16 +45,17 @@ public class Curso {
 		return encontrado;
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Crea un nuevo curso vacío
+	 */
 	public Curso() {
 		listaAlumnos= new ArrayList<Persona>();
 	}
 	
-	/*
-	 * Documentar y crear test unitario
-	 * */
+	/**
+	 * Comprueba cuántos alumnos tiene el curso
+	 * @return Número de alumnos del curso
+	 */
 	public Integer numeroAlumnos() {return listaAlumnos.size();}
 	
 	
