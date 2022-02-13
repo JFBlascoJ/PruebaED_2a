f348545ee43a827ecce64baecaafd762fe134fb0
jfblascoj <jfblascoj@gmail.com>
2022-02-13 16:59:03 +0100

    Formateado de clases Persona y Curso

--- src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
@@ -4,61 +4,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Curso {
-	
+
 	private List<Persona> listaAlumnos;
 
 	/**
 	 * Elimina del curso a la persona con el DNI indicado por usuario
+	 * 
 	 * @param dni DNI de la persona a borrar
 	 * @throws Exception cuando el DNI no tiene la longitud adecuada
 	 */
 	public void eliminarAlumno(String dni) throws Exception {
-		if(dni.length()==9) {//comprobar la longitud del dni
-			listaAlumnos.remove(new Persona(dni, "", "")); //solo hace falta el dni
-		}else {
+		if (dni.length() == 9) {// comprobar la longitud del dni
+			listaAlumnos.remove(new Persona(dni, "", "")); // solo hace falta el dni
+		} else {
 			throw new Exception("El dni no tiene la longitud adecuada");
 		}
 	}
-	
+
 	/**
 	 * Añade una nueva persona al curso
+	 * 
 	 * @param p Persona a añadir
 	 */
 	public void aniadirAlumno(Persona p) {
 		listaAlumnos.add(p);
 	}
-	
+
 	/**
 	 * Comprueba si el DNI pasado por parámetro existe dentro del curso en cuestión
+	 * 
 	 * @param dni DNI a comprobar
 	 * @return True si existe. En caso contratio, retornará false
 	 */
 	public Boolean estaRegistrado(String dni) {
-		int i =0;
-		Boolean encontrado=false;
-		while (!encontrado && i<listaAlumnos.size()) {
-			if(listaAlumnos.get(i).getDni().equals(dni)) {
-				encontrado=true;
+		int i = 0;
+		Boolean encontrado = false;
+		while (!encontrado && i < listaAlumnos.size()) {
+			if (listaAlumnos.get(i).getDni().equals(dni)) {
+				encontrado = true;
 			}
 			i++;
 		}
 		return encontrado;
 	}
-	
+
 	/**
 	 * Crea un nuevo curso vacío
 	 */
 	public Curso() {
-		listaAlumnos= new ArrayList<Persona>();
+		listaAlumnos = new ArrayList<Persona>();
 	}
-	
+
 	/**
 	 * Comprueba cuántos alumnos tiene el curso
+	 * 
 	 * @return Número de alumnos del curso
 	 */
-	public Integer numeroAlumnos() {return listaAlumnos.size();}
-	
-	
+	public Integer numeroAlumnos() {
+		return listaAlumnos.size();
+	}
+
 	public void mostrarTodos() {
 		for (Persona persona : listaAlumnos) {
 			System.out.println(persona);
--- src/com/iescomercio/ed/bloque2/repaso/modelo/Persona.java
+++ src/com/iescomercio/ed/bloque2/repaso/modelo/Persona.java
@@ -1,17 +1,20 @@
 package com.iescomercio.ed.bloque2.repaso.modelo;
 
-public class Persona{
-	
+public class Persona {
+
 	private String dni;
 	private String nombre;
 	private String apellido1;
-	
-	public Persona() {}
-	
+
+	public Persona() {
+	}
+
 	/**
-	 * Constructor para crear una nueva persona pasando todos los atributos como parámetro
-	 * @param dni DNI de la persona
-	 * @param nombre Nombre de la persona
+	 * Constructor para crear una nueva persona pasando todos los atributos como
+	 * parámetro
+	 * 
+	 * @param dni       DNI de la persona
+	 * @param nombre    Nombre de la persona
 	 * @param apellido1 Apellido de la persona
 	 */
 	public Persona(String dni, String nombre, String apellido1) {
@@ -19,63 +22,67 @@ public class Persona{
 		this.nombre = nombre;
 		this.apellido1 = apellido1;
 	}
-	
+
 	/**
 	 * Obtiene el DNI de la persona
+	 * 
 	 * @return Cadena con el DNI de la persona
 	 */
 	public String getDni() {
 		return dni;
 	}
-	
-	
+
 	/**
 	 * Cambia el DNI de la persona en cuestión
+	 * 
 	 * @param dni Nuevo DNI
 	 * @throws Exception si el último carácter del nuevo DNI no es una letra
 	 */
 	public void setDni(String dni) throws Exception {
-		//comprobacion de si el ultimo caracter es una letra
-		if(Character.isLetter(dni.charAt(dni.length() - 1))) {
-			this.dni=dni;
-		}else {
+		// comprobacion de si el ultimo caracter es una letra
+		if (Character.isLetter(dni.charAt(dni.length() - 1))) {
+			this.dni = dni;
+		} else {
 			throw new Exception("El ultimo caracter introducido no es una letra");
 		}
 	}
-	
+
 	/**
 	 * Obtiene el nombre de la persona en cuestión
+	 * 
 	 * @return Cadea con el nombre de la persona
 	 */
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	/**
 	 * Cambia el nombre de la persona
+	 * 
 	 * @param nombre Nuevo nombre
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	/**
 	 * Obtiene el apellido de la persona en cuestión
+	 * 
 	 * @return Cadena con el apellido de la persona
 	 */
 	public String getApellido1() {
 		return apellido1;
 	}
-	
+
 	/**
 	 * Cambia el apellido de la persona
+	 * 
 	 * @param apellido1 Nuevo apellido
 	 */
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
 	}
-	
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -83,6 +90,7 @@ public class Persona{
 		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -104,6 +112,5 @@ public class Persona{
 	public String toString() {
 		return "Persona [dni=" + dni + ", nombre=" + nombre + "]";
 	}
-	
 
 }
